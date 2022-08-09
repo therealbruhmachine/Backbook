c96d04a32729a67cce70c799ec9539d13e160e81
Dylan Corona <106871151+therealbruhmachine@users.noreply.github.com>
2022-08-09 10:19:19 -0500

    Created index

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Shartbook | Welcome to Shartbook!</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+header {
+  background-color: #3B5998;
+  height: 50px;
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+  font-size: 10px;
+  color: white;
+}
+
+nav {
+  float: left;
+  width: 20%;
+  height: 300px;
+  background: #FFFFFF;
+  padding: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  float: left;
+  padding: 20px;
+  width: 50%;
+  background-color: #f1f1f1;
+  height: 200px;
+  font-size: 9px;
+}
+
+section::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+footer {
+  background-color: #FFFFFF;
+  padding: 10px;
+  text-align: center;
+  font-size: 12px;
+  color: black;
+}
+
+@media (max-width: 600px) {
+  nav, article {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
+</head>
+<body>
+
+<header>
+  <img src="hlogo.png">
+  <h2>Shartbook header</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <p>[sidebar goes here]</p>
+    </ul>
+  </nav>
+  
+  <article>
+<h2>Shartbook is an online directory that connects people through social networks around the web.</h2>
+<!--Site is in progress-->
+    <p>The site is still in the making but you can go to the Discord here!</p>
+    
+<p>The site is curently open to only me as we are testing some things but we hope to open soon.</p>
+
+    <p>You can use shartbook to:</p>
+    <ul class="bulletpoints">
+    <li><span><p>Connect with friends and family around the world.</p></span></li>
+    <li><span><p>Get to know new people.</p></span></li>
+    <li><span><p>Idfk.</p></span></li>
+
+
+  </article>
+</section>
+
+<footer>
+  <p><a href="">placeholder</a></p>
+  <p>a Dylan Corona production<p>
+
+<p>Shartbook © 2006</p>
+</footer>
+
+</body>
+</html>
